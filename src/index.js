3d43d1ba981de230afcc85c7ba11a6d1d6c19cf8
venkatesh <vengets@gmail.com>
2021-03-13 19:19:33 +0530

    With all Basic Board components

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import Game from './component/Game';
+
+ReactDOM.render(
+    <Game />,
+    document.getElementById('root')
+);
